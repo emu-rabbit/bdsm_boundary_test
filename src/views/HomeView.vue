7e4d3b2d77e355e6a39b5cc33d8d569bd724a6a8
絵夢羽さ沂 <mausu2526@gmail.com>
2026-06-30 15:29:27 +0800

    Fix: 調整主頁輔助入口

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,24 +26,6 @@ const settingsEntrance = computed(() => routeById.value.get('settings'));
 <template>
   <section class="home-route">
     <div class="home-ambient" aria-hidden="true" />
-    <header class="home-topbar">
-      <p class="home-eyebrow">兔子的祕密檔案</p>
-      <button
-        v-if="settingsEntrance"
-        class="settings-action"
-        type="button"
-        aria-label="開啟設定"
-        :title="settingsEntrance.label"
-        @click="emit('navigate', settingsEntrance.id)"
-      >
-        <svg viewBox="0 0 24 24" focusable="false" aria-hidden="true">
-          <path
-            d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.52a2 2 0 0 1-1 1.72l-.15.1a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.38a2 2 0 0 0-.73-2.73l-.15-.1a2 2 0 0 1-1-1.72v-.52a2 2 0 0 1 1-1.72l.15-.1a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2Z"
-          />
-          <circle cx="12" cy="12" r="3" />
-        </svg>
-      </button>
-    </header>
 
     <div class="home-stage">
       <img
@@ -82,15 +64,20 @@ const settingsEntrance = computed(() => routeById.value.get('settings'));
 
     <div class="home-footer-actions">
       <button
+        v-if="settingsEntrance"
+        class="soft-link-action"
+        type="button"
+        @click="emit('navigate', settingsEntrance.id)"
+      >
+        {{ settingsEntrance.label }}
+      </button>
+      <button
         v-if="aboutEntrance"
         class="soft-link-action"
         type="button"
         @click="emit('navigate', aboutEntrance.id)"
       >
         {{ aboutEntrance.label }}
-      </button>
-      <button class="soft-link-action" type="button" @click="emit('navigate', 'story')">
-        再聽兔子說一次開場
       </button>
     </div>
   </section>
